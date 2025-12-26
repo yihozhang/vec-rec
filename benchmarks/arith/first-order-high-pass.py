20a38c3e21bf08c850c3a673f43202b1537625d6
Yihong Zhang <yz489@cs.washington.edu>
2025-12-25 21:16:12 -0800

    more benchmarks

--- benchmarks/arith/first-order-high-pass.py
+++ benchmarks/arith/first-order-high-pass.py
@@ -1,1 +1,47 @@
-# f(x) = a * (f(x-1) + g(x) - g(x-1))
+"""First-order high-pass filter benchmark.
+
+f(x) = a * (f(x-1) + g(x) - g(x-1))
+"""
+
+from vecrec.expr import *
+from vecrec.expr import Type
+from vecrec.transform import *
+from vecrec.codegen import (
+	CodeGen,
+	generate_and_run_benchmark,
+	generate_benchmark,
+	instantiate_kernels,
+)
+
+
+def main():
+	lanes = 512
+	a = 0.8
+
+	program = Recurse(
+		TIKernel([0, a], Type.Arith),
+		Convolve(TIKernel([a, -a], Type.Arith), Var("g", Type.Arith)),
+	)
+
+	transforms = Seq(
+		Optional(Seq(Preorder(Try(Factorize())), Preorder(Try(ConstantFold)))),
+		Optional(
+			Seq(
+				RepeatUpTo(3, Dilate(), Try(Preorder(ConstantFold))),
+				Any(Dilate(), Delay()),
+			),
+		),
+		AnnotateLanes(lanes),
+		PushDownConvertLanes(),
+	)
+
+	results = transforms.apply_signal(program)
+	codegen = CodeGen()
+	benchmark_result = generate_and_run_benchmark(
+		codegen, results, ["k" + str(i) for i in range(len(results))], True
+	)
+	print(benchmark_result)
+
+
+if __name__ == "__main__":
+	main()
---
+++ benchmarks/arith/first-order-ti.py
@@ -0,0 +1,40 @@
+# Fibonacci: f(x) = f(x-1) + f(x-2)
+
+from vecrec.expr import *
+from vecrec.expr import Type
+from vecrec.transform import *
+from vecrec.codegen import (
+	CodeGen,
+	generate_and_run_benchmark,
+	generate_benchmark,
+	instantiate_kernels,
+)
+
+
+def main():
+	for program in [
+        Recurse(TIKernel([0, 1.0, 1.0], Type.Arith), Num(1.0, Type.Arith)),
+        Recurse(TIKernel([0, 1.0, 1.0], Type.Arith), Var("x", Type.Arith)),
+    ]:
+            transforms = Seq(
+                Optional(
+                    Seq(
+                        RepeatUpTo(3, Dilate(), Try(Preorder(ConstantFold))),
+                        Any(Dilate(), Delay()),
+                    ),
+                ),
+                AnnotateLanes(512),
+                PushDownConvertLanes(),
+            )
+
+            results = transforms.apply_signal(program)
+            codegen = CodeGen()
+            benchmark_result = generate_and_run_benchmark(
+                codegen, results, ["k" + str(i) for i in range(len(results))], True
+            )
+            print(benchmark_result)
+
+
+if __name__ == "__main__":
+	main()
+
